c455b278d2c407cdfc4f001883af75415ec33fff
vaibhavbharti <vaibhavbharti.cse@geu.ac.in>
2021-06-18 11:06:14 +0530

    Photo Updated

--- assets/js/dynamicTitle.js
+++ assets/js/dynamicTitle.js
@@ -20,7 +20,7 @@ window.onload = function () {
   document.addEventListener("visibilitychange", function (e) {
     if (!document.hidden) {
       document.title = pageTitle;
-      favicon.href = "./assets/images/dp_male.svg";
+      favicon.href = "./assets/images/vaibhavbharti.jpeg";
     }
   });
 
@@ -28,7 +28,7 @@ window.onload = function () {
     blinkEvent = setInterval(() => {
       if (document.title === attentionMessage) {
         document.title = pageTitle;
-        favicon.href = "./assets/images/dp_male.svg";
+        favicon.href = "./assets/images/vaibhavbharti.jpeg";
       } else {
         document.title = attentionMessage;
         favicon.href = "./assets/images/folded.png";
